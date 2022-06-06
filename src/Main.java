d83a190f787fc3f791c40e68f65a9679b4c89f40
lyjb1 <lyjb1@rose-hulman.edu>
2022-06-05 22:42:59 -0400

    initial test

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        double price;
 
-    //hi
+        Scanner s = new Scanner(System.in);
+        double tax = .05;
+        System.out.println("Please enter a price: ");
+        price = s.nextDouble();
+        System.out.println("Your tax is " + price*tax + " and your total is: " + (price + price*tax));
 
     }
 }
